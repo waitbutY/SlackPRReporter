95a9104deb2bfe813cbd950949cec6ab5ce65a79
waitbutY <jacob.ackerman145@gmail.com>
2026-04-13 23:00:13 -0500

    chore: project scaffolding

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+};
+
+export default config;
